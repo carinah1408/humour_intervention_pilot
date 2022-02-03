1cd4a3fc7ef95416dec2f889292d812c7c91bbbf
Carina Hoerst <carinahoerst@Carinas-MacBook-Pro.local>
2022-02-03 17:51:08 +0000

    completion outlier efficacy condition

--- scripts/pilot.R
+++ scripts/pilot.R
@@ -38,7 +38,7 @@ hist(pilot$attention_1_2)
 # outlier detection
 attention_1_1 <- pilot$attention_1_1
 attention_1_1_mad <- Routliers::outliers_mad(x = attention_1_1)
-attention_1_1_mad
+attention_1_1_mad # no outliers
 
 attention_1_2 <- pilot$attention_1_2
 attention_1_2_mad <- Routliers::outliers_mad(x= attention_1_2)
@@ -70,9 +70,33 @@ attention_2_1_mad
 
 attention_2_2 <- pilot$attention_2_2
 attention_2_2_mad <- Routliers::outliers_mad(x = attention_2_2)
-attention_2_2_mad
+attention_2_2_mad # 1 outlier, id 033
+
+
 
 # descriptives
+
+
+
+pilot %>%
+  summarise(mean = mean(attention_2_2), sd = sd(attention_2_2))
+
+# without outlier
+pilot %>%
+  filter(id != "033") %>%
+  summarise(mean = mean(attention_2_2), sd = sd(attention_2_2), n = n())
+
+# by group
+pilot %>%
+  group_by(affiliation) %>%
+  summarise(mean = mean(attention_2_2), sd = sd(attention_2_2))
+
+# without outlier (Centre)
+pilot %>%
+  filter(id != "033") %>%
+  group_by(affiliation) %>%
+  summarise(mean = mean(attention_2_2), sd = sd(attention_2_2))
+
 
 # scenario 3 (interrupted music)
 hist(pilot$attention_3_1)
@@ -81,13 +105,29 @@ hist(pilot$attention_3_2)
 # outlier detection
 attention_3_1 <- pilot$attention_3_1
 attention_3_1_mad <- Routliers::outliers_mad(x=attention_3_1)
-attention_3_1_mad
+attention_3_1_mad # no outliers
 
 attention_3_2 <- pilot$attention_3_2
 attention_3_2_mad <- Routliers::outliers_mad(x=attention_3_2)
-attention_3_2_mad
+attention_3_2_mad # no outliers
 
 # descriptives
+pilot %>%
+  summarise(mean = mean(attention_3_1), sd = sd(attention_3_1))
+
+# by group
+pilot %>%
+  group_by(affiliation) %>%
+  summarise(mean = mean(attention_3_1), sd = sd(attention_3_1))
+
+
+pilot %>%
+  summarise(mean = mean(attention_3_2), sd = sd(attention_3_2))
+
+# by group
+pilot %>%
+  group_by(affiliation) %>%
+  summarise(mean = mean(attention_3_2), sd = sd(attention_3_2))
 
 # scenario 4 (interrupted signs)
 hist(pilot$attention_4_1)
@@ -96,13 +136,29 @@ hist(pilot$attention_4_2)
 # outlier detection
 attention_4_1 <- pilot$attention_4_1
 attention_4_1_mad <- Routliers::outliers_mad(x=attention_4_1)
-attention_4_1_mad
+attention_4_1_mad # no outliers
 
 attention_4_2 <- pilot$attention_4_2
 attention_4_2_mad <- Routliers::outliers_mad(x=attention_4_2)
-attention_4_2_mad
+attention_4_2_mad # no outliers
 
 # descriptives
+pilot %>%
+  summarise(mean = mean(attention_4_1), sd = sd(attention_4_1))
+
+# by group
+pilot %>%
+  group_by(affiliation) %>%
+  summarise(mean = mean(attention_4_1), sd = sd(attention_4_1))
+
+
+pilot %>%
+  summarise(mean = mean(attention_4_2), sd = sd(attention_4_2))
+
+# by group
+pilot %>%
+  group_by(affiliation) %>%
+  summarise(mean = mean(attention_4_2), sd = sd(attention_4_2))
 
 # scenario 5 (interrupted milkshake)
 hist(pilot$attention_5_1)
@@ -111,8 +167,26 @@ hist(pilot$attention_5_2)
 # outlier detection
 attention_5_1 <- pilot$attention_5_1
 attention_5_1_mad <- Routliers::outliers_mad(x=attention_5_1)
-attention_5_1_mad
+attention_5_1_mad # no outliers
 
 attention_5_2 <- pilot$attention_5_2
 attention_5_2_mad <- Routliers::outliers_mad(x=attention_5_2)
-attention_5_2_mad
+attention_5_2_mad # no outliers
+
+# descriptives
+pilot %>%
+  summarise(mean = mean(attention_5_1), sd = sd(attention_5_1))
+
+# by group
+pilot %>%
+  group_by(affiliation) %>%
+  summarise(mean = mean(attention_5_1), sd = sd(attention_5_1))
+
+
+pilot %>%
+  summarise(mean = mean(attention_5_2), sd = sd(attention_5_2))
+
+# by group
+pilot %>%
+  group_by(affiliation) %>%
+  summarise(mean = mean(attention_5_2), sd = sd(attention_5_2))
